d9858bc94a0c9f5649d56293f487760cae55d0f0
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-06-02 19:26:18 -0300

    setup initial ChickenNFT contract

---
+++ blockchain/contracts/ChickenNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract ChickenNFT {
+    constructor() {
+        console.log("This is my chicken NFT mint contract!");
+    }
+}
