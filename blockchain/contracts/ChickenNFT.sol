d3cdacc32bcc95b34f1db9a6a32dd15498359914
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-06-02 21:57:35 -0300

    create mint nft contract function and deploy script

--- blockchain/contracts/ChickenNFT.sol
+++ blockchain/contracts/ChickenNFT.sol
@@ -2,10 +2,31 @@
 
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract ChickenNFT {
-    constructor() {
+contract ChickenNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("SquareNFT", "SQUARE") {
         console.log("This is my chicken NFT mint contract!");
+    }
+
+    function makeAChickenNFT() public {
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/DCOQ");
+
+        _tokenIds.increment();
+
+        console.log(
+            "A NFT w/ ID %s has been minted to %s",
+            newItemId,
+            msg.sender
+        );
     }
 }
--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -1,17 +1,17 @@
 const main = async () => {
   const nftContractFactory = await hre.ethers.getContractFactory('ChickenNFT')
   const nftContract = await nftContractFactory.deploy()
-
   await nftContract.deployed()
   console.log('Contract deployed to:', nftContract.address)
 
   let mintNFT = await nftContract.makeAChickenNFT()
-
   await mintNFT.wait()
-
+  console.log("Minted NFT #1")
+  
   let mintSecondNFT = await nftContract.makeAChickenNFT()
-
   await mintSecondNFT.wait()
+  console.log("Minted NFT #2")
+
 }
 
 const runMain = async () => {
