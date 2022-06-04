640aabf40d8efc8921612ad6f7f023690969af9e
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-06-04 14:01:42 -0300

    add mint contract function to web app

--- web/src/components/Header/Logo.tsx
+++ web/src/components/Header/Logo.tsx
@@ -1,16 +1,8 @@
-import { Flex, Heading, Image } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 
 export function Logo() {
   return (
     <Flex alignItems="center">
-      {/* <Image
-        src="/logo.png"
-        alt="logo"
-        boxSize={["2rem", "2rem", "3rem"]}
-        mr="3"
-        objectFit="cover"
-      /> */}
-
       <Heading fontSize={["0", "3xl", "4xl"]} fontWeight="bold">
         Chicken NFT
       </Heading>
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,68 @@
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Button, Flex, Heading, Text, useToast } from "@chakra-ui/react";
+import { useState } from "react";
+import { ethers } from "ethers";
 import Head from "next/head";
+
+import chickenNFTABI from "../config/ChickenNFT.json";
 
 import { ConnectWalletModal } from "../components/ConnectWalletModal";
 import { WalletProfileModal } from "../components/WalletProfileModal";
 
+const CONTRACT_ADDRESS = "0x85a18211e19ba6e21EF739d228FEA670058B0699";
+
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toast = useToast();
+
+  async function handleMint() {
+    setIsLoading(true);
+
+    try {
+      const { ethereum } = window as any;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          chickenNFTABI.abi,
+          signer
+        );
+
+        console.log("pop wallet to pay gas...");
+        let nftTxn = await connectedContract.makeAChickenNFT();
+
+        console.log("mining...please wait.");
+        await nftTxn.wait();
+
+        toast({
+          title: "NFT successfully minted",
+          description: `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`,
+          status: "success",
+          duration: 10000,
+          isClosable: true,
+          position: "top-right",
+        });
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (err) {
+      console.log({ err });
+
+      toast({
+        title: "Mint error, try again",
+        description: (err as Error).message,
+        status: "error",
+        duration: 4000,
+        isClosable: true,
+        position: "top-right",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <Flex w="100%" h="100%" direction="column" alignItems="center" pb="8">
       <Head>
@@ -38,6 +96,7 @@ export default function Home() {
         </Heading>
 
         <Text
+          mb="6"
           fontSize={["xl", "xl", "2xl"]}
           color="gray.300"
           fontWeight="400"
@@ -45,6 +104,18 @@ export default function Home() {
         >
           Each unique. Each beautiful. Discover your NFT today.
         </Text>
+
+        <Button
+          onClick={handleMint}
+          isLoading={isLoading}
+          loadingText="Minting"
+          px="12"
+          py="8"
+          colorScheme="orange"
+          fontSize="2xl"
+        >
+          Mint NFT
+        </Button>
       </Flex>
     </Flex>
   );
